f3ff6582f51ab1f79d5068b2b839208e67f20169
Samantha Mathews <samathews@google.com>
2024-09-10 16:37:22 +0000

    commit terraform-metastore-federation-tags

--- mmv1/third_party/terraform/services/dataprocmetastore/resource_dataproc_metastore_federation_test.go
+++ mmv1/third_party/terraform/services/dataprocmetastore/resource_dataproc_metastore_federation_test.go
@@ -1,4 +1,3 @@
-<% autogen_exception -%>
 package dataprocmetastore_test
 
 import (
@@ -11,18 +10,19 @@ import (
 
 func TestAccMetastoreFederation_tags(t *testing.T) {
 	t.Parallel()
-
-	name := "tf-test-metastore-" + acctest.RandString(t, 10)
+	
+        context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
 	tagKey := acctest.BootstrapSharedTestTagKey(t, "metastore-federations-tagkey")
 	tagValue := acctest.BootstrapSharedTestTagValue(t, "metastore-federations-tagvalue", tagKey)
 
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
-		CheckDestroy:             testAccCheckMetastoreFederationDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccMetastoreFederationTags(name, map[string]string{org + "/" + tagKey: tagValue}),
+				Config: testAccMetastoreFederationTags(context, map[string]string{tagKey: tagValue}),
 			},
 			{
 				ResourceName:            "google_metastore_federation.my_metastore",
@@ -30,52 +30,13 @@ func TestAccMetastoreFederation_tags(t *testing.T) {
 				ImportStateVerify:       true,
 				ImportStateVerifyIgnore: []string{"tags"},
 			},
-			{
-				Config: testAccMetastoreFederationTags_allowDestroy(name, map[string]string{org + "/" + tagKey: tagValue}),
-			},
 		},
 	})
 }
 
-func testAccMetastoreFederationTags(context map[string]interface{}, tags map[string]string) string {
+func testAccMetastoreFederationTags(context map[string]interface{},tags map[string]string) string {
 
-	r := fmt.Sprintf(`
-	resource "google_dataproc_metastore_federation" "default" {
-          location      = "us-central1"
-          federation_id = "metastore-fed"
-          version       = "3.1.2"
-
-          backend_metastores {
-            rank           = "1"
-            name           = google_dataproc_metastore_service.default.id
-            metastore_type = "DATAPROC_METASTORE" 
-         }
-       }
-
-       resource "google_dataproc_metastore_service" "default" {
-         service_id = "metastore-srv-new"
-         location   = "us-central1"
-         tier       = "DEVELOPER"
-
-
-         hive_metastore_config {
-           version           = "3.1.2"
-           endpoint_protocol = "GRPC"
-         }
-	  tags = {`, context)
-
-	l := ""
-	for key, value := range tags {
-		l += fmt.Sprintf("%q = %q\n", key, value)
-	}
-
-	l += fmt.Sprintf("}\n}")
-	return r + l
-}
-
-func testAccMetastoreFederationTags_allowDestroy(context map[string]interface{}, tags map[string]string) string {
-
-	r := fmt.Sprintf(`
+	r := acctest.Nprintf(`
 	resource "google_dataproc_metastore_federation" "default" {
           location      = "us-central1"
           federation_id = "metastore-fed"
